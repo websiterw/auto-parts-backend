171199da7034000b9c43f3f2e34a9c9ebb053d57
websiterw <websiteemailrw@gmail.com>
2026-07-28 20:05:13 +0200

    Update teamController.js

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -1,52 +1,52 @@
 const User = require('../models/User');
 const Commission = require('../models/Commission');
-const Investment = require('../models/Investment'); // <-- ADD THIS
+const Investment = require('../models/Investment');
 
 exports.getTeamData = async (req, res) => {
   try {
     const userId = req.user.id;
 
-    // --- Level 1 (direct referrals) ---
+    // Level 1
     const level1Users = await User.find({ referredBy: userId }).select('accountNumber balance createdAt');
     const level1Ids = level1Users.map(u => u._id);
 
-    // Add total investment amount for each level1 user
     const level1WithAmount = await Promise.all(level1Users.map(async (u) => {
-      const investments = await Investment.find({ userId: u._id, isActive: true });
-      const totalInvested = investments.reduce((sum, inv) => sum + inv.price, 0);
+      // Get the first investment (by date) for this user
+      const firstInvestment = await Investment.findOne({ userId: u._id }).sort({ purchasedAt: 1 });
+      const amount = firstInvestment ? firstInvestment.price : 0;
       return {
         ...u.toObject(),
-        amount: totalInvested
+        amount
       };
     }));
 
-    // --- Level 2 ---
+    // Level 2
     const level2Users = await User.find({ referredBy: { $in: level1Ids } }).select('accountNumber');
     const level2Ids = level2Users.map(u => u._id);
 
     const level2WithAmount = await Promise.all(level2Users.map(async (u) => {
-      const investments = await Investment.find({ userId: u._id, isActive: true });
-      const totalInvested = investments.reduce((sum, inv) => sum + inv.price, 0);
+      const firstInvestment = await Investment.findOne({ userId: u._id }).sort({ purchasedAt: 1 });
+      const amount = firstInvestment ? firstInvestment.price : 0;
       return {
         ...u.toObject(),
-        amount: totalInvested
+        amount
       };
     }));
 
-    // --- Level 3 ---
+    // Level 3
     const level3Users = await User.find({ referredBy: { $in: level2Ids } }).select('accountNumber');
     const level3Ids = level3Users.map(u => u._id);
 
     const level3WithAmount = await Promise.all(level3Users.map(async (u) => {
-      const investments = await Investment.find({ userId: u._id, isActive: true });
-      const totalInvested = investments.reduce((sum, inv) => sum + inv.price, 0);
+      const firstInvestment = await Investment.findOne({ userId: u._id }).sort({ purchasedAt: 1 });
+      const amount = firstInvestment ? firstInvestment.price : 0;
       return {
         ...u.toObject(),
-        amount: totalInvested
+        amount
       };
     }));
 
-    // Total rewards (sum of all commissions earned)
+    // Total rewards (sum of all commissions)
     const commissions = await Commission.find({ referrerId: userId });
     const totalRewards = commissions.reduce((sum, c) => sum + c.amount, 0);
 
