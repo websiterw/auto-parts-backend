fba78a22357fa2ee0683731525e9df2c89fb416c
websiterw <websiteemailrw@gmail.com>
2026-07-29 19:20:53 +0200

    Update teamController.js

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -10,50 +10,21 @@ exports.getTeamData = async (req, res) => {
     const level1Users = await User.find({ referredBy: userId }).select('accountNumber balance createdAt');
     const level1Ids = level1Users.map(u => u._id);
 
-    const level1WithAmount = await Promise.all(level1Users.map(async (u) => {
-      // Get the first investment (by date) for this user
-      const firstInvestment = await Investment.findOne({ userId: u._id }).sort({ purchasedAt: 1 });
-      const amount = firstInvestment ? firstInvestment.price : 0;
-      return {
-        ...u.toObject(),
-        amount
-      };
-    }));
-
     // Level 2
     const level2Users = await User.find({ referredBy: { $in: level1Ids } }).select('accountNumber');
     const level2Ids = level2Users.map(u => u._id);
 
-    const level2WithAmount = await Promise.all(level2Users.map(async (u) => {
-      const firstInvestment = await Investment.findOne({ userId: u._id }).sort({ purchasedAt: 1 });
-      const amount = firstInvestment ? firstInvestment.price : 0;
-      return {
-        ...u.toObject(),
-        amount
-      };
-    }));
-
     // Level 3
     const level3Users = await User.find({ referredBy: { $in: level2Ids } }).select('accountNumber');
-    const level3Ids = level3Users.map(u => u._id);
 
-    const level3WithAmount = await Promise.all(level3Users.map(async (u) => {
-      const firstInvestment = await Investment.findOne({ userId: u._id }).sort({ purchasedAt: 1 });
-      const amount = firstInvestment ? firstInvestment.price : 0;
-      return {
-        ...u.toObject(),
-        amount
-      };
-    }));
-
-    // Total rewards (sum of all commissions)
+    // Total rewards = sum of all commissions earned by this user
     const commissions = await Commission.find({ referrerId: userId });
     const totalRewards = commissions.reduce((sum, c) => sum + c.amount, 0);
 
     res.json({
-      level1: level1WithAmount,
-      level2: level2WithAmount,
-      level3: level3WithAmount,
+      level1: level1Users,
+      level2: level2Users,
+      level3: level3Users,
       totalUsers: level1Users.length + level2Users.length + level3Users.length,
       totalRewards
     });
