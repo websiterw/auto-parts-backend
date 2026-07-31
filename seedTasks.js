1be6b6a20922ed1435c851887016f5d2170e3519
websiterw <websiteemailrw@gmail.com>
2026-07-31 18:41:07 +0200

    Update seedTasks.js

--- seedTasks.js
+++ seedTasks.js
@@ -9,8 +9,8 @@ const seedTasks = async () => {
   await Task.deleteMany();
   const tasks = [
     { name: 'Invite 3 Level 1 investors', level: 1, target: 3, reward: 2000 },
-    { name: 'Invite 10 Level 1 investors', level: 1, target: 10, reward: 5000 },
-    { name: 'Invite 30 Level 1 investors', level: 1, target: 30, reward: 10000 }
+    { name: 'Invite 10 Level 1 investors', level: 2, target: 10, reward: 5000 },
+    { name: 'Invite 30 Level 1 investors', level: 3, target: 30, reward: 10000 }
   ];
   await Task.insertMany(tasks);
   console.log('✅ Tasks seeded!');
