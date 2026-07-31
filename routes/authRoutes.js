5d4a84c834dd71aafa7972ad8037b9949da0872c
websiterw <websiteemailrw@gmail.com>
2026-07-31 19:10:24 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const auth = require('../middleware/auth');
 
+// Public routes
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+// Protected routes
 router.get('/me', auth, authController.getMe);
+
+// NEW: Change password for logged-in user
+router.post('/change-password', auth, authController.changePassword);
 
 module.exports = router;
